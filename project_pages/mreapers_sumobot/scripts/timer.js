3f3e10beaf8b346d1e438b946c68ea23290e0c0d
fynnbehnke <fynn.behnke@gmail.com>
2024-06-05 22:35:33 +0200

    added images

--- project_pages/mreapers_sumobot/scripts/timer.js
+++ project_pages/mreapers_sumobot/scripts/timer.js
@@ -5,19 +5,19 @@ let isRunning = false;
 
 function startCountdown() {
     countdownTimer = setInterval(updateTimer, updateInterval);
-    document.getElementById("buttonImage").src = "https://lh3.googleusercontent.com/drive-viewer/AKGpihbfkKqbG4rIH0NJoRxe-CmieVMz0HMYJdhmf_eL_RozatL1UaoRLfRz0usb_c8PGSgJSDZYUR8ZFxZLPbNfnvI8D7tD=s2560";
+    document.getElementById("buttonImage").src = "../../../img/play-button.png";
 }
 
 function pauseCountdown() {
     clearInterval(countdownTimer);
-    document.getElementById("buttonImage").src = "https://lh3.googleusercontent.com/drive-viewer/AKGpihay6anqWh4vmrBuS0CNARYIcm5ipP0_X3o3EE-oVj6vx5-K_wB2ukUMdWJo_tthTL-RlAA0t0uMmLBLz8EERK8Pwp1mZA=s2560";
+    document.getElementById("buttonImage").src = "../../../img/pause-button.png";
 }
 
 function resetCountdown() {
     clearInterval(countdownTimer);
     timeleft = 180;
     updateTimer();
-    document.getElementById("buttonImage").src = "https://lh3.googleusercontent.com/drive-viewer/AKGpihay6anqWh4vmrBuS0CNARYIcm5ipP0_X3o3EE-oVj6vx5-K_wB2ukUMdWJo_tthTL-RlAA0t0uMmLBLz8EERK8Pwp1mZA=s2560";
+    document.getElementById("buttonImage").src = "../../../img/play-button.png";
     isRunning = false;
 }
 
