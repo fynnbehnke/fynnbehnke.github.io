12214769cb4a0f891a55fb7f99164b6211e23a2e
fynnbehnke <fynn.behnke@gmail.com>
2024-10-20 17:45:30 +0200

    footer test

---
+++ src/scripts/footer.js
@@ -0,0 +1,14 @@
+// Dynamically load the footer content
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('./footer.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+});
